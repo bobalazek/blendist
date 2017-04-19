a9f23cebd51be8bacde33be53650cc30fee9f5bf
Borut <bobalazek124@gmail.com>
2017-04-19 18:52:59 +0200

    Some clenaup

--- src/core/config.js
+++ src/core/config.js
@@ -5,6 +5,10 @@ const argv = require('yargs').argv;
 let config = {};
 const env = argv.env || 'dev';
 
+const configPath = path.join(
+    __dirname,
+    '../config/config.json'
+);
 const configDistPath = path.join(
     __dirname,
     '../config/config.dist.json'
@@ -13,33 +17,27 @@ const envConfigPath = path.join(
     __dirname,
     '../config/config_' + env + '.json'
 );
+const packagePath = path.join(
+    __dirname,
+    '../../package.json'
+);
 
 if (fs.existsSync(configDistPath)) {
-    let configContent = fs.readFileSync(configDistPath);
+    const configContent = fs.readFileSync(configDistPath);
     config = JSON.parse(''+configContent);
 } else {
     // Package data
-    let packageContent = fs.readFileSync(
-        path.join(
-            __dirname,
-            '../../package.json'
-        )
-    );
-    let packageData = JSON.parse(''+packageContent);
+    const packageContent = fs.readFileSync(packagePath);
+    const packageData = JSON.parse(''+packageContent);
 
     // Global config data
-    let configContent = fs.readFileSync(
-        path.join(
-            __dirname,
-            '../config/config.json'
-        )
-    );
-    let configData = JSON.parse(''+configContent);
+    const configContent = fs.readFileSync(configPath);
+    const configData = JSON.parse(''+configContent);
 
     // Environment config data
     if (fs.existsSync(envConfigPath)) {
-        let envConfigContent = fs.readFileSync(envConfigPath);
-        let envConfigData = JSON.parse(''+envConfigContent);
+        const envConfigContent = fs.readFileSync(envConfigPath);
+        const envConfigData = JSON.parse(''+envConfigContent);
         configData = Object.assign(configData, envConfigData);
     }
 
